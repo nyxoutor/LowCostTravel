ea28f802708347bc65616b183ba697cf5bf497e0
Tony <troux33@hotmail.fr>
2015-05-29 12:36:03 +0200

    Ajout modif vols

---
+++ LowCostTravel/Fm_Modif_ Vols.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace LowCostTravel
+{
+    partial class Fm_Modif__Vols
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lb_Modif_Vol = new System.Windows.Forms.Label();
+            this.Lb_Date_Depart = new System.Windows.Forms.Label();
+            this.Lb_Arrivee_Vols = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.Lb_Depart = new System.Windows.Forms.Label();
+            this.Cb_Depart = new System.Windows.Forms.ComboBox();
+            this.Lb_Arrivee = new System.Windows.Forms.Label();
+            this.Cb_Arrivee = new System.Windows.Forms.ComboBox();
+            this.Lb_Places = new System.Windows.Forms.Label();
+            this.Tb_Places = new System.Windows.Forms.TextBox();
+            this.Lb_Prix = new System.Windows.Forms.Label();
+            this.Tb_Prix = new System.Windows.Forms.TextBox();
+            this.Annuler = new System.Windows.Forms.Button();
+            this.Bt_Modifier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lb_Modif_Vol
+            // 
+            this.Lb_Modif_Vol.AutoSize = true;
+            this.Lb_Modif_Vol.Location = new System.Drawing.Point(255, 32);
+            this.Lb_Modif_Vol.Name = "Lb_Modif_Vol";
+            this.Lb_Modif_Vol.Size = new System.Drawing.Size(76, 13);
+            this.Lb_Modif_Vol.TabIndex = 0;
+            this.Lb_Modif_Vol.Text = "Vol à modifier :";
+            // 
+            // Lb_Date_Depart
+            // 
+            this.Lb_Date_Depart.AutoSize = true;
+            this.Lb_Date_Depart.Location = new System.Drawing.Point(103, 86);
+            this.Lb_Date_Depart.Name = "Lb_Date_Depart";
+            this.Lb_Date_Depart.Size = new System.Drawing.Size(105, 13);
+            this.Lb_Date_Depart.TabIndex = 1;
+            this.Lb_Date_Depart.Text = "Date/Heure Départ :";
+            // 
+            // Lb_Arrivee_Vols
+            // 
+            this.Lb_Arrivee_Vols.AutoSize = true;
+            this.Lb_Arrivee_Vols.Location = new System.Drawing.Point(102, 131);
+            this.Lb_Arrivee_Vols.Name = "Lb_Arrivee_Vols";
+            this.Lb_Arrivee_Vols.Size = new System.Drawing.Size(106, 13);
+            this.Lb_Arrivee_Vols.TabIndex = 2;
+            this.Lb_Arrivee_Vols.Text = "Date/Heure Arrivée :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(258, 80);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(258, 125);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // Lb_Depart
+            // 
+            this.Lb_Depart.AutoSize = true;
+            this.Lb_Depart.Location = new System.Drawing.Point(163, 178);
+            this.Lb_Depart.Name = "Lb_Depart";
+            this.Lb_Depart.Size = new System.Drawing.Size(45, 13);
+            this.Lb_Depart.TabIndex = 5;
+            this.Lb_Depart.Text = "Départ :";
+            // 
+            // Cb_Depart
+            // 
+            this.Cb_Depart.FormattingEnabled = true;
+            this.Cb_Depart.Location = new System.Drawing.Point(258, 175);
+            this.Cb_Depart.Name = "Cb_Depart";
+            this.Cb_Depart.Size = new System.Drawing.Size(121, 21);
+            this.Cb_Depart.TabIndex = 6;
+            // 
+            // Lb_Arrivee
+            // 
+            this.Lb_Arrivee.AutoSize = true;
+            this.Lb_Arrivee.Location = new System.Drawing.Point(162, 220);
+            this.Lb_Arrivee.Name = "Lb_Arrivee";
+            this.Lb_Arrivee.Size = new System.Drawing.Size(46, 13);
+            this.Lb_Arrivee.TabIndex = 7;
+            this.Lb_Arrivee.Text = "Arrivée :";
+            // 
+            // Cb_Arrivee
+            // 
+            this.Cb_Arrivee.FormattingEnabled = true;
+            this.Cb_Arrivee.Location = new System.Drawing.Point(258, 217);
+            this.Cb_Arrivee.Name = "Cb_Arrivee";
+            this.Cb_Arrivee.Size = new System.Drawing.Size(121, 21);
+            this.Cb_Arrivee.TabIndex = 8;
+            // 
+            // Lb_Places
+            // 
+            this.Lb_Places.AutoSize = true;
+            this.Lb_Places.Location = new System.Drawing.Point(104, 263);
+            this.Lb_Places.Name = "Lb_Places";
+            this.Lb_Places.Size = new System.Drawing.Size(104, 13);
+            this.Lb_Places.TabIndex = 9;
+            this.Lb_Places.Text = "Nombres de places :";
+            // 
+            // Tb_Places
+            // 
+            this.Tb_Places.Location = new System.Drawing.Point(258, 263);
+            this.Tb_Places.Name = "Tb_Places";
+            this.Tb_Places.Size = new System.Drawing.Size(121, 20);
+            this.Tb_Places.TabIndex = 10;
+            // 
+            // Lb_Prix
+            // 
+            this.Lb_Prix.AutoSize = true;
+            this.Lb_Prix.Location = new System.Drawing.Point(172, 303);
+            this.Lb_Prix.Name = "Lb_Prix";
+            this.Lb_Prix.Size = new System.Drawing.Size(30, 13);
+            this.Lb_Prix.TabIndex = 11;
+            this.Lb_Prix.Text = "Prix :";
+            // 
+            // Tb_Prix
+            // 
+            this.Tb_Prix.Location = new System.Drawing.Point(258, 300);
+            this.Tb_Prix.Name = "Tb_Prix";
+            this.Tb_Prix.Size = new System.Drawing.Size(121, 20);
+            this.Tb_Prix.TabIndex = 12;
+            // 
+            // Annuler
+            // 
+            this.Annuler.Location = new System.Drawing.Point(223, 350);
+            this.Annuler.Name = "Annuler";
+            this.Annuler.Size = new System.Drawing.Size(75, 23);
+            this.Annuler.TabIndex = 13;
+            this.Annuler.Text = "Annuler";
+            this.Annuler.UseVisualStyleBackColor = true;
+            // 
+            // Bt_Modifier
+            // 
+            this.Bt_Modifier.Location = new System.Drawing.Point(322, 350);
+            this.Bt_Modifier.Name = "Bt_Modifier";
+            this.Bt_Modifier.Size = new System.Drawing.Size(75, 23);
+            this.Bt_Modifier.TabIndex = 14;
+            this.Bt_Modifier.Text = "Modifier";
+            this.Bt_Modifier.UseVisualStyleBackColor = true;
+            // 
+            // Fm_Modif__Vols
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 396);
+            this.Controls.Add(this.Bt_Modifier);
+            this.Controls.Add(this.Annuler);
+            this.Controls.Add(this.Tb_Prix);
+            this.Controls.Add(this.Lb_Prix);
+            this.Controls.Add(this.Tb_Places);
+            this.Controls.Add(this.Lb_Places);
+            this.Controls.Add(this.Cb_Arrivee);
+            this.Controls.Add(this.Lb_Arrivee);
+            this.Controls.Add(this.Cb_Depart);
+            this.Controls.Add(this.Lb_Depart);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Lb_Arrivee_Vols);
+            this.Controls.Add(this.Lb_Date_Depart);
+            this.Controls.Add(this.Lb_Modif_Vol);
+            this.Name = "Fm_Modif__Vols";
+            this.Text = "Fm_Modif__Vols";
+            this.Load += new System.EventHandler(this.Fm_Modif__Vols_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lb_Modif_Vol;
+        private System.Windows.Forms.Label Lb_Date_Depart;
+        private System.Windows.Forms.Label Lb_Arrivee_Vols;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label Lb_Depart;
+        private System.Windows.Forms.ComboBox Cb_Depart;
+        private System.Windows.Forms.Label Lb_Arrivee;
+        private System.Windows.Forms.ComboBox Cb_Arrivee;
+        private System.Windows.Forms.Label Lb_Places;
+        private System.Windows.Forms.TextBox Tb_Places;
+        private System.Windows.Forms.Label Lb_Prix;
+        private System.Windows.Forms.TextBox Tb_Prix;
+        private System.Windows.Forms.Button Annuler;
+        private System.Windows.Forms.Button Bt_Modifier;
+    }
+}
---
+++ LowCostTravel/Fm_Modif_ Vols.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LowCostTravel
+{
+    public partial class Fm_Modif__Vols : Form
+    {
+        public Fm_Modif__Vols()
+        {
+            InitializeComponent();
+        }
+
+        private void Fm_Modif__Vols_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
